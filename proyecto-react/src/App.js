2b0ff172c6ee613aa1f4b8b66409b01f62467eca
joskeiner urdaneta <joskeiner.simosa9@gmail.com>
2022-11-07 01:50:50 -0300

    carrito

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -3,14 +3,19 @@ import  CarouselFadeExample from './componentes/Carrusel';
 import Card from './componentes/Card';
 import Navbar from './componentes/navbar/Navbar';
 import PieDePag from './componentes/footer/Footer';
-
+import React, { useState } from 'react';
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    //cuando esta true lo pasa a false y vice versa
+    setCount(count + 1)
+  }
   return (
   
     <>
-    <Navbar/>
+    <Navbar count={count}/>
     <div className='item'>
     < CarouselFadeExample/>
     </div>
@@ -21,6 +26,7 @@ function App() {
             titulo_card = 'Cubre Alfombra'
             texto_card = 'Cubre alfombra de goma Vapren para habitáculo Audi A3'
             precio_card= '500.23'
+            handleClick={handleClick}
         />
       </div>
 
@@ -29,6 +35,7 @@ function App() {
           titulo_card = 'Deposito Refrigerante'
           texto_card = 'Deposito recuperador agua con tapa para Ford Ka'
           precio_card= '899,99'
+          handleClick={handleClick}
           />
           
           <div className='fijo'>
@@ -37,6 +44,7 @@ function App() {
                     titulo_card = 'Filtro de Aceite Vw'
                     texto_card = 'Filtro de aceite original Volkswagen 3.0 v6'
                     precio_card= '23,78'
+                    handleClick={handleClick}
                     />
           </div>
           <div className='fijo' >
@@ -45,6 +53,7 @@ function App() {
                     titulo_card = 'Retrovisor para golf'
                     texto_card = 'Retrovisor original Volkswagen 3.0 v6'
                     precio_card= '1.123,78'
+                    handleClick={handleClick}
                     />
           </div>
           <div className='fijo'><Card
@@ -52,6 +61,7 @@ function App() {
                     titulo_card = 'Pistones Originales'
                     texto_card = 'pistones originales para toyota'
                     precio_card= '999,78'
+                    handleClick={handleClick}
                 /></div>
             <div className='fijo'>
             <Card
@@ -59,6 +69,7 @@ function App() {
                       titulo_card = 'cubiertas 150'
                       texto_card = 'cubiertas pirelli  90 90 100 pista  sin camara '
                       precio_card= '2.333,78'
+                      handleClick={handleClick}
 
                       />
             </div>
@@ -68,6 +79,7 @@ function App() {
                       titulo_card = 'cubiertas traseras '
                       texto_card = 'cuebiertas para motos enduro 150 '
                       precio_card= '9.323,78'
+                      handleClick={handleClick}
 
                       />
             </div>
@@ -77,6 +89,7 @@ function App() {
                       titulo_card = 'juego de cubiertas Enduro'
                       texto_card = 'juego de cubiertas pirelli para motos enduro trasera  90 120 120   delantera 90 100 100'
                       precio_card= '15.586,08'
+                      handleClick={handleClick}
 
                       />
             </div>
@@ -86,6 +99,7 @@ function App() {
                       titulo_card = 'aceite para autos Mobil'
                       texto_card = 'aceite semi-simtetico 30/50'
                       precio_card= '684,87'
+                      handleClick={handleClick}
                       />
             </div>
 
