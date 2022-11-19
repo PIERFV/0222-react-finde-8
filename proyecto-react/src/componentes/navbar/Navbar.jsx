76407eb80a6ba9ec669c637afce45f123faff32c
joskeiner urdaneta <joskeiner.simosa9@gmail.com>
2022-11-18 23:11:58 -0300

    modificacion de count

--- proyecto-react/src/componentes/navbar/Navbar.jsx
+++ proyecto-react/src/componentes/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled'
 import BurguerButton from './BurguerButton'
-import  Modal from '../Ventana/modal'
 
- function Navbar({ count}){ 
+
+ function Navbar({ size, setShow}){ 
     //se usa el metodo useState para actualizar el componente burguerbutton
     const [clicked, setClicked] = useState(false)
     const handleClick = () => {
@@ -18,20 +18,22 @@ import  Modal from '../Ventana/modal'
             <img className="card-img"
                 src={require('../../imagenes/logo.jpg')}
                 alt= 'logo de la empresa '
+                onClick={() => setShow(true)}
+                id='987'
                 />
             <div  className={`links ${clicked ? 'active' : ''}`}>
-            <div className='numer'><span >{count}</span></div>
-                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" 
-                                                                                width="2.5rem" height="2rem" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">{count}
+            <div className='numer'><span >{size}</span></div>
+                <a href="#"   onClick={() => setShow(false)}><svg xmlns="http://www.w3.org/2000/svg" 
+                                                                                width="2.5rem" height="2rem" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">{size}
                                                                                 <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0
                                                                                      0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3
                                                                                       0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0
                                                                                        5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2
                                                                                         2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
                                                                               </svg></a>
-                <a  href="https://developer.mozilla.org/es/docs/Web/CSS/z-index">inicio</a>
-                <a href="https://developer.mozilla.org/es/docs/Web/CSS/z-index">productos</a>
-                <a href="https://developer.mozilla.org/es/docs/Web/CSS/z-index">Nosotros</a>
+                <a  href="#987" onClick={() => setShow(true)}>inicio</a>
+                <a href="#345" onClick={() => setShow(true)}>productos</a>
+                <a href="#123" onClick={() => setShow(true)}>Nosotros</a>
               
             </div>
             <div className='burguer'>
